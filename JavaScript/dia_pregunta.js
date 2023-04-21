c7d154f506c46049c34334f3091401e66c3430df
Iamfrans <1323544@senati.pe>
2023-04-21 11:34:35 -0500

    js intento 3

--- JavaScript/dia_pregunta.js
+++ JavaScript/dia_pregunta.js
@@ -1,3 +1,13 @@
-var resultado[
-document.write('Hola');
-]]
+var countDDate = new Date(),
+            endday = new Date(), days =24;
+            countDDate.setDate(countDDate.getDate() - (countDDate.getDay() + 2)%7);
+            endday.setDate(countDDate.getDate() + days);
+            endday.getTime();
+            countDiv = document.getElementById('countdown');
+        var x = setInterval(function(){
+            var now = new Date().getTime();
+            var daterest = endday - now;
+            var days = Math.floor(daterest / (1000 * 60 * 60 * 24));
+            
+            countDiv.innerHTML = "falan 23 dias";
+        });
