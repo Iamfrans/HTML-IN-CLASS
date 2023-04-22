d5b6963f358db814af42629ddbb789b81b7a3308
Iamfrans <1323544@senati.pe>
2023-04-22 09:54:36 -0500

    js ejercice 1.0

--- JavaScript/ejercice.js
+++ JavaScript/ejercice.js
@@ -1,5 +1,6 @@
-const event = new Date("August 19, 1975, 23:15:30");
+const evento = new Date('April 22, 2023 23:15:30');
 evento.setDate(24);
 console.log(evento.getDate());
+//Expected output: 24 
 evento.setDate(32);
-console.log(evento.getDate());
+console.log(evento.getDate());  
